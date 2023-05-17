e283e132b14069743e93b9d3141e23b77e709159
Hardikpanchasara <hardikpatel97253@gmail.com>
2023-05-17 21:47:31 +0530

    homepage setup complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,8 +61,9 @@ const Header = (props) => {
               <User>
                 <a>
                   <img src="/images/user.svg" alt="" />
-                  <span>Me</span>
+                  <span>Me
                   <img src="/images/down-icon.svg" alt="" />
+                  </span>
                 </a>
 
                 <SignOut>
